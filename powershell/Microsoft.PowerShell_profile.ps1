8ec3f0da9ec39658dac52f1cd2eb3d0f89221e22
Taylor Vesely <dreddor@dreddor.net>
2019-04-11 15:07:46 -0700

    Windows: Install powershell profile

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+# Make powershell terminal behave like default bash
+Set-PSReadLineOption -EditMode Emacs
+
+# Use WSL vi to edit items in powershell.
+function vi ($File){
+    $File = $File -replace "\\", "/"
+    $File = $File -replace "^C:", "/mnt/c"
+    bash -c "vi $File"
+}
+
+Function makefast {
+    Set-MpPreference -DisableRealtimeMonitoring $true
+}
+
+Function makeslow {
+    Set-MpPreference -DisableRealtimeMonitoring $false
+}
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
