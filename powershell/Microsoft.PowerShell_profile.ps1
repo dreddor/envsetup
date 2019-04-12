fab8f88f7aacf75ad72f4ab85db19286d6203ac3
Taylor Vesely <dreddor@dreddor.net>
2019-04-12 10:20:55 -0700

    Windows: Add 'Get-Command' alias 'which'

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -21,3 +21,5 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+New-Alias which Get-Command
